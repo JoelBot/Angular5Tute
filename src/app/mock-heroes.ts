12840452dde70be4d2797e32f1601c9e1d365791
Joel Lonneker <joel.lonneker@gmail.com>
2018-02-18 18:45:06 -0800

    Section 4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', power: 'Dog whistle' },
+  { id: 12, name: 'Narco', power: 'Apple slicer' },
+  { id: 13, name: 'Bombasto', power: 'Install chill' },
+  { id: 14, name: 'Celeritas', power: 'Berzerker' },
+  { id: 15, name: 'Magneta', power: 'Psyiotics' },
+  { id: 16, name: 'RubberMan', power: 'Beer Brewing' },
+  { id: 17, name: 'Dynama', power: 'Clocksmith' },
+  { id: 18, name: 'Dr IQ', power: 'Reddit admin' },
+  { id: 19, name: 'Magma', power: 'Mixologist' },
+  { id: 20, name: 'Tornado', power: 'Fireballs' }
+];
